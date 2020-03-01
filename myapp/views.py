b6f409d3150bac1f82cf2949a78db159af01ea18
amr11mahmoud <amrmahmoudfcb11@gmail.com>
2020-03-01 19:47:56 +0200

    adding some testing methods and classes

--- myapp/views.py
+++ myapp/views.py
@@ -4,14 +4,19 @@ from django.http import HttpResponse,Http404,HttpResponseRedirect
 from .models import Question,Choice
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
+from .. import Django_Full_Project
 
 class HomeView(generic.ListView):
     template_name = 'myapp/home.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+            """
+        return Question.objects.filter( pub_date__lte=timezone.now() ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
