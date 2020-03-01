ec686d7a7274f5aacf8503d08588043c15cf8b6a
amr11mahmoud <amrmahmoudfcb11@gmail.com>
2020-03-01 17:09:38 +0200

    just before moving to generic views

--- Django_Full_Project/settings.py
+++ Django_Full_Project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp.apps.MyappConfig'
 ]
 
 MIDDLEWARE = [
@@ -107,6 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
+#Africa/Cairo
 
 USE_I18N = True
 
--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render ,get_object_or_404
 
 # Create your views here.
 
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from .models import Question,Choice
 
+from django.urls import reverse
 
 
 def home(request):
-    return HttpResponse(" hello, world ")
+    try :
+        latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    except:
+        raise Http404
+
+    template = ('myapp/home.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request,template,context)
+
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'myapp/detail.html', {'question': question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'myapp/results.html', {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question,pk = question_id)
+    try:
+        # get the choice that entered through the form
+        selected_choice = question.choice_set.get(pk = request.POST['choice'])
+
+    # it will enter the except case if no choice picked
+    except (KeyError, Choice.DoesNotExist):
+        return render(request,"myapp/detail.html",{
+            "question" : question,
+            'error_message' : " you didn't selected a choice "
+        })
+    else:
+        # increase the votes for that choice by 1 inside the DB
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('myapp:results', args=(question.id,)))
+
